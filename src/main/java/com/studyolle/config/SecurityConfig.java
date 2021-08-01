8ad6554b2a67fade1c7ad738e1987b6314c272bf
LeeGiCheol <asdqwek200@naver.com>
2021-08-01 17:36:11 +0900

    로그인 / 로그아웃

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -18,7 +18,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
                         "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+        ;
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll()
+        ;
+
+        http.logout()
+                .logoutSuccessUrl("/logout")
+        ;
+
+
     }
 
     @Override
