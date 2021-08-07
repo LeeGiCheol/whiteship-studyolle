ea2592d8f562f2c4e1c3c8b83ab1e4588f3bb09a
LeeGiCheol <asdqwek200@naver.com>
2021-08-07 17:22:13 +0900

    닉네임 변경

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -2,8 +2,8 @@ package com.studyolle.account;
 
 import com.studyolle.domain.Account;
 import com.studyolle.mail.ConsoleMailSender;
-import com.studyolle.settings.Notifications;
-import com.studyolle.settings.Profile;
+import com.studyolle.settings.form.Notifications;
+import com.studyolle.settings.form.Profile;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.mail.SimpleMailMessage;
@@ -117,5 +117,11 @@ public class AccountService implements UserDetailsService {
     public void updateNotification(Account account, Notifications notifications) {
         modelMapper.map(notifications, account);
         accountRepository.save(account);
+    }
+
+    public void updateNickname(Account account, String nickname) {
+        account.setNickname(nickname);
+        accountRepository.save(account);
+        login(account);
     }
 }
