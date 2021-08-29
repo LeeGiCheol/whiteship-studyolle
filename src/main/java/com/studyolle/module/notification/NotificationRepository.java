e3abd321213c13bf32407b682f93225cbe1e5925
LeeGiCheol <asdqwek200@naver.com>
2021-08-29 21:16:53 +0900

    HandlerInterceptor를 이용해 알림 시 아이콘 색 변경

--- src/main/java/com/studyolle/module/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/module/notification/NotificationRepository.java
@@ -1,6 +1,10 @@
 package com.studyolle.module.notification;
 
+import com.studyolle.module.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
+
 }
